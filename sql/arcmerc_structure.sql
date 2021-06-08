09178d821dd5105c609ce815d848e7dbbce1d62f
cressidagp <gcpigni@gmail.com>
2021-06-08 18:51:35 -0300

    Added basic structure.

---
+++ sql/arcmerc_structure.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS `merc`;
+
+CREATE TABLE `merc` (
+  `guid` int(6) unsigned NOT NULL DEFAULT '0',
+  `name` varchar(21) NOT NULL DEFAULT '',
+  `slot1entry` int(6) unsigned NOT NULL DEFAULT '0',
+  `slot2entry` int(6) unsigned NOT NULL DEFAULT '0',
+  `slot3entry` int(6) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`guid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='MercSystem';
