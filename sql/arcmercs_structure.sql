e6cd5f5a5278777c7f2a3d0b2fcf2e853a53d3bb
cressidagp <gcpigni@gmail.com>
2021-06-08 18:58:13 -0300

    Switched slot from int6 to int10.

--- sql/arcmercs_structure.sql
+++ sql/arcmercs_structure.sql
@@ -3,8 +3,8 @@ DROP TABLE IF EXISTS `merc`;
 CREATE TABLE `merc` (
   `guid` int(6) unsigned NOT NULL DEFAULT '0',
   `name` varchar(21) NOT NULL DEFAULT '',
-  `slot1entry` int(6) unsigned NOT NULL DEFAULT '0',
-  `slot2entry` int(6) unsigned NOT NULL DEFAULT '0',
-  `slot3entry` int(6) unsigned NOT NULL DEFAULT '0',
+  `slot1entry` int(10) unsigned NOT NULL DEFAULT '0',
+  `slot2entry` int(10) unsigned NOT NULL DEFAULT '0',
+  `slot3entry` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`guid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='MercSystem';
