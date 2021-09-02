701d8336e237a2f6584fe20a62f72fc54a0b9fd0
cressidagp <gcpigni@gmail.com>
2021-09-02 14:32:25 -0300

    Database structure dump.

--- sql/arcmercs_character_structure.sql
+++ sql/arcmercs_character_structure.sql
@@ -1,3 +1,19 @@
+/*
+SQLyog Community v13.1.6 (64 bit)
+MySQL - 5.7.35-log : Database - arc_char
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `merc` */
+
 DROP TABLE IF EXISTS `merc`;
 
 CREATE TABLE `merc` (
@@ -8,3 +24,10 @@ CREATE TABLE `merc` (
   `slot3entry` int(10) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`guid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Mercenary System';
+
+/*Data for the table `merc` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
