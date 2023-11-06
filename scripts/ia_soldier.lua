63562b87a9af019900b73c03b758e0561efb55c7
cressidagp <gcpigni@gmail.com>
2023-11-06 15:19:30 -0300

    Improved 'Soldier' AI.

--- scripts/ia_soldier.lua
+++ scripts/ia_soldier.lua
@@ -1,10 +1,20 @@
--- local EMOTE_ONESHOT_EAT = 7
--- local EMOTE_ONESHOT_FLEX = 23
--- local EMOTE_ONESHOT_DANCE = 94
--- local UNIT_FIELD_SUMMONEDBY = 0x0006 + 0x0008
+--local EMOTE_ONESHOT_TALK = 1
+--local EMOTE_ONESHOT_EAT = 7
+--local EMOTE_ONESHOT_FLEX = 23
+--local EMOTE_ONESHOT_DANCE = 94
+--local SPELL_CHARGE = 100
+--local SPELL_DRINK = 430
+--local SPELL_EAT = 433
+--local UNIT_FIELD_SUMMONEDBY = 0x0006 + 0x0008
+--local UNIT_FIELD_TARGET = 0x0006 + 0x000C
+--local UNIT_FIELD_BYTES_0 = 0x0006 + 0x0011
+--local UNIT_FIELD_BYTES_1 = 0x0006 + 0x0044
+--local UNIT_FIELD_BYTES_2 = 0x0006 + 0x0074 
 
 ARCMERCS = {}
 ARCMERCS.SOLDIER = {}
+
+math.randomseed(os.time()); math.random(); math.random(); math.random();
 
 function ARCMERCS.SOLDIER.OnSpawn( unit, event )
 
@@ -36,12 +46,19 @@ end
 
 function ARCMERCS.SOLDIER.OnAIUpdate( unit, event )
 
-	print("AI update")
-
 	--
 	-- merc its idle
 	--
 
+	if unit:GetAIState() == 0 then
+	
+		if unit:GetByteValue( 0x0006 + 0x0044, 0 ) ~= 1 then
+		
+			-- sit on the ground
+			unit:SetByteValue( 0x0006 + 0x0044, 0, 1 )
+		end
+		
+	end
 
 	-- 
 	-- merc its attacking
@@ -51,6 +68,7 @@ function ARCMERCS.SOLDIER.OnAIUpdate( unit, event )
 	
 		local target = unit:GetUInt64Value( 0x0006 + 0x000C )
 		
+		-- dont bother with this if no target
 		if target == nil then return; end
 	
 		if ARCMERCS[tostring(unit)].SOLDIER_STANCE == 1 then
@@ -81,23 +99,40 @@ function ARCMERCS.SOLDIER.OnAIUpdate( unit, event )
 
 	if unit:GetAIState() == 4 then
 	
+		if unit:GetByteValue( 0x0006 + 0x0044, 0 ) ~= 0 then
+		
+			-- stand up
+			unit:SetByteValue( 0x0006 + 0x0044, 0, 0 )
+			
+		end
+	
+		-- dont bother with this if merc its moving
+		if unit:IsCreatureMoving() == true then return; end
+		
+		-- dont bother with this if eating of drinking
+		if unit:HasAura( 430 ) or unit:HasAura( 433 ) then return; end
+		
+		if unit:GetHealthPct() < 35 then
+		
+			unit:CastSpell( 433 )
+			
+		end
+	
 		local f = math.random( 0, 100 )
 		
-		if( f > 0 and f <= 2 ) then
+		if( f > 0 and f <= 15 ) then
 		
-			unit:CastSpell( 433 ) -- eat
-			
-		elseif( f > 2 and f <= 5 ) then
+			unit:Emote( 1, 0 ) -- talk
+	
+		elseif( f > 20 and f <= 22 ) then
 		
 			unit:Emote( 23, 0 ) -- flex
 			
-		elseif( f > 5 and f <= 8 ) then
+		elseif( f > 31 and f <= 33 ) then
 		
 			unit:Emote( 16, 0 ) -- dance
-			
-		elseif( f > 8 and f <= 10 ) then
-		
-			unit:CastSpell( 430 ) -- drink
+
+		else
 		
 		end	
 	
